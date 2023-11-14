f1fc2a22802c57380422015db29ec6dad7e943e8
Helinä Lähteenmäki <helina.lahteenmaki@gmail.com>
2023-11-14 13:08:17 +0200

    Week 2 ex. 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_kortin_saldoa(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+    
+    def test_saldo_vähenee_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_saldo_ei_muutu_jos_ei_rahaa(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_palauttaa_true_jos_rahat_riittävät(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(500))
+
+    def test_palauttaa_false_jos_rahat_eivät_riitä(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(1500))
