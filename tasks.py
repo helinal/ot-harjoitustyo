147428b16752c50a911d8ed95db4eebc28130f8e
Helinä Lähteenmäki <helina.lahteenmaki@gmail.com>
2023-11-21 21:06:40 +0200

    Added coverage and more invoke tasts

--- tasks.py
+++ tasks.py
@@ -1,5 +1,19 @@
 from invoke import task
+from subprocess import call
+from sys import platform
 
 @task
 def start(ctx):
     ctx.run("python3 src/app.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
